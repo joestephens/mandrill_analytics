505f5863f142e5d6f6a2555ce4419122d3df592b
joestephens <joe.stephens@live.co.uk>
2016-08-21 23:17:48 +0100

    first failing feature test for statistics page

---
+++ spec/feature_test_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe 'Analytics page' do
+  it 'should have total number of emails sent' do
+    params = {
+      address: "barney@lostmy.name",
+      email_type: "Shipment",
+      event: "send",
+      timestamp: 1432820696
+    }
+
+    2.times do
+      Webhook.create(
+        address: params[:address],
+        email_type: params[:email_type],
+        event: params[:event],
+        timestamp: Time.at(params[:timestamp])
+      )
+    end
+
+    visit '/'
+
+    expect(page).to have_content("Total number of emails sent: 2")
+  end
+
+  it 'should have total number of emails opened' do
+
+  end
+
+  it 'should have total number of clicks' do
+
+  end
+
+  it 'should have open rate per email type' do
+
+  end
+
+  it 'should have click rate per email type' do
+
+  end
+end
